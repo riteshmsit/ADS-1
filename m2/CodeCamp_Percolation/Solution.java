fbb04f9f558ee0a0319b7ca29da3e3c4de458c43
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-25 19:58:25 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- m2/CodeCamp_Percolation/Solution.java
+++ m2/CodeCamp_Percolation/Solution.java
@@ -29,26 +29,26 @@ class Percolation {
 	// }
 	public void open(int row, int col) {
 		if (grid[row][col]) {
-		    return;
+            return;
         }
         grid[row][col] = true;
-		if (row == 0) {
-			wqu.union(convert(row, col), size * size);
-		}
-		if (row == size - 1) {
-			wqu.union(convert(row,col), size * size + 1);
-		}
-		if ((row > 0) && grid[row - 1][col]) {
-            wqu.union(convert(row,col), convert(row,col + 1));
+        if (row == 0) {
+            wqu.union(convert(row, col), size * size);
         }
-        if ((row < size - 1) &&  grid[row + 1][col]) {
-            wqu.union(convert(row,col),convert(row + 1,col));
+        if (row == size - 1) {
+            wqu.union(convert(row, col), size * size + 1);
         }
-        if (((col > 0)) && grid[row][col - 1]) {
-            wqu.union(convert(row,col),convert(row,col - 1));
+        if (row < size - 1 && grid[row + 1][col]) { //bottom element
+            wqu.union(convert(row, col), convert(row + 1, col));
         }
-        if ((col < size - 1) && grid[row][col + 1]) {
-            wqu.union(convert(row,col),convert(row,col + 1));
+        if (row > 0 && grid[row - 1][col]) { //top element
+            wqu.union(convert(row, col), convert(row - 1, col));
+        }
+        if (col > 0 && grid[row][col - 1]) { //left element
+            wqu.union(convert(row, col), convert(row, col - 1));
+        }
+        if (col < size - 1 && grid[row][col + 1]) { //right element
+            wqu.union(convert(row, col), convert(row, col + 1));
         }
 	}
 	public int convert(final int i, final int j) {
