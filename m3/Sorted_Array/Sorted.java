0db7c1a9ade0308fe7605f216706e280af8602ee
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-09-26 23:32:31 +0530

    "check_style_score -> 3 of 3 passed. style score: 1/1 "

--- m3/Sorted_Array/Sorted.java
+++ m3/Sorted_Array/Sorted.java
@@ -20,23 +20,32 @@ class Sorted {
     public String concat(int[] arr1, int[] arr2) {
         int i = 0,j = 0,k = 0;
         int[] res = new int[arr1.length + arr2.length];
-        while (i < arr1.length && j < arr2.length) {
-            if (arr1[i] < arr2[j]) {
-                res[k] = arr1[i];
-                k++;
-                i++;
-            } else {
-                res[k] = arr2[j];
-                j++;
-                k++;
+        while (i < arr1.length + arr2.length) {
+            if (j < arr1.length && k < arr2.length) {
+                if (arr1[j] <= arr2[k]) {
+                    res[i++] = arr1[j++];
+                    // k++;
+                    // j++;
+                } else {
+                    res[i++] = arr2[k++];
+                    // i++;
+                    // k++;
+                }
+
+            } else if (j < arr1.length) {
+                res[i++] = arr1[j++];
+                // i++;
+                // j++;
+            } else if (k < arr2.length) {
+                res[i++] = arr2[k++];
             }
         }
-        while (i < arr1.length) {
-            res[k++] = arr1[i++];
-        }
-        while (j < arr2.length) {
-            res[k++] = arr2[j++];
-        }
+        // while (i < arr1.length) {
+        //     res[k++] = arr1[i++];
+        // }
+        // while (j < arr2.length) {
+        //     res[k++] = arr2[j++];
+        // }
         return Arrays.toString(res).replaceAll("[\\]\\[ ]", "");
     }
 }
