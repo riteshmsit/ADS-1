5de565b92fa28d64fb6f636e8b59846c4b2e5b5e
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-10-01 12:10:56 +0530

    "check_style_score -> 2 of 2 passed. style score: 1/1 "

--- m7/7.1 Steque/Solution.java
+++ m7/7.1 Steque/Solution.java
@@ -1,80 +1,50 @@
 import java.util.Scanner;
-class LinkedList<E> {
-    private Node head, tail;
-    class Node {
-        private E data;
-        private Node next;
-        public Node(E item) {
-            this.data = item;
-            this.next = null;
-        }
+/**.
+ * { item_description }
+ */
+final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+
     }
-    public LinkedList() {
-        head = null;
-        tail = null;
-    }
-    public void push(E item) {
-        Node newnode = new Node(item);
-        if (head == null) {
-            tail = newnode;
-        }
-        newnode.next = head;
-        head = newnode;
-    }
-    public void pop() {
-        if (head == null) {
-            System.out.println("Steque is empty.");
-            return;
-        }
-        head = head.next;
-        if (head == null) 
-            tail = head;
-    }
-    public void enqueue(E item) {
-        if (head == null) {
-            push(item);
-            return;
-        }
-        Node newnode = new Node(item);
-        tail.next = newnode;
-        newnode.next = null;
-        tail = newnode;
-    }
-    public void print() {
-        Node temp = head;
-        String str = "";
-        if (head == null) return;
-        while(temp.next != null) {
-            str += temp.data + ", ";
-            temp = temp.next;
-            
-        }
-        str += temp.data;
-        System.out.println(str);
-    }
-}
-class Solution {
-    public static void main(String[] args) {
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            LinkedList<Integer> ll = new LinkedList<Integer>();
-            while(sc.hasNext()) {
-                String[] str = sc.nextLine().split(" ");
-                switch (str[0]) {
-                    case "push":
-                        ll.push(Integer.parseInt(str[1]));
+        int nooftestcases = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < nooftestcases; i++) {
+            Steque obj = new Steque();
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                if (!line.isEmpty()) {
+                    String[] ins = line.split(" ");
+                    switch (ins[0]) {
+                        case "enqueue":
+                    obj.addLast(Integer.parseInt(ins[1]));
                         break;
-                    case "pop":
-                        ll.pop();
+                        case "push":
+                    obj.pushFront(Integer.parseInt(ins[1]));
+                    break;
+                        case "pop":
+                        if (obj.isEmpty()) {
+                System.out.println("Steque is empty.");
+                break;
+                        } else {
+                            obj.deleteLast();
+                            break;
+                        }
+                        default:
                         break;
-                    case "enqueue":
-                        ll.enqueue(Integer.parseInt(str[1]));
-                        break;
-                    default:
-                        break;
+                    }
+                } else {
+                    System.out.println();
+                    break;
                 }
-                ll.print();
             }
         }
     }
