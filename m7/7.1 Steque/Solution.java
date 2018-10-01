d632d224b45f4df01622f4b70206223630866c0a
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-10-01 10:45:44 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- m7/7.1 Steque/Solution.java
+++ m7/7.1 Steque/Solution.java
@@ -4,32 +4,34 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int nooftestcases = Integer.parseInt(sc.nextLine());
 		int i = 0;
-		
 		while (i < nooftestcases) {
 			Steque<Integer> obj = new Steque<Integer>();
 			while (sc.hasNext()) {
-				String[] ins = sc.nextLine().split(" ");
-				switch(ins[0]) {
-					case "enqueue":
-					obj.pushAtEnd(Integer.parseInt(ins[1]));
-					obj.print();
-					break;	
-					case "push":
-					obj.insertFront(Integer.parseInt(ins[1]));
-					int j = 0;
-					if (j > 0) {
+				String line = sc.nextLine();
+				if (!line.isEmpty()) {
+					String[] ins = sc.nextLine().split(" ");
+					switch(ins[0]) {
+						case "enqueue":
+						obj.pushAtEnd(Integer.parseInt(ins[1]));
 						obj.print();
+						break;	
+						case "push":
+						obj.insertFront(Integer.parseInt(ins[1]));
+						int j = 0;
+						if (j > 0) {
+							obj.print();
+						}
+						break;
+						case "pop":
+						obj.deleteBack(Integer.parseInt(ins[1]));
+						obj.print();
+						break;
+						default:
+						break;
 					}
-					break;
-					case "pop":
-					obj.deleteBack(Integer.parseInt(ins[1]));
-					obj.print();
-					break;
-					default:
-					break;
 				}
+				i++;
 			}
-			i++;
 		}
 	}
 }
