7dbf260b38612f1845d3d72ec1e79795504e012d
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-10-01 12:02:28 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- m7/7.1 Steque/Solution.java
+++ m7/7.1 Steque/Solution.java
@@ -1,44 +1,49 @@
 import java.util.Scanner;
-class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
+final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int nooftestcases = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < nooftestcases; i++) {
-		//while (i < nooftestcases) {
-			Steque obj = new Steque();
-			while (sc.hasNext()) {
-				String line = sc.nextLine();
-				if (!line.isEmpty()) {
-					String[] ins = line.split(" ");
-					switch (ins[0]) {
-						case "enqueue":
-					obj.addLast(Integer.parseInt(ins[1]));
-						break;
-						case "push":
-					obj.pushFront(Integer.parseInt(ins[1]));
-					break;
-						case "pop":
-						if (obj.isEmpty()) {
-				System.out.println("Steque is empty.");
-				break;
-						} else {
-							obj.deleteLast();
-							break;
-						}
-						default:
-						break;
-					}
-				} else {
-					System.out.println();
-					break;
-				}
-			}
-		}
-	}
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nooftestcases = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < nooftestcases; i++) {
+        //while (i < nooftestcases) {
+            Steque obj = new Steque();
+            while (sc.hasNext()) {
+                String line = sc.nextLine();
+                if (!line.isEmpty()) {
+                    String[] ins = line.split(" ");
+                    switch (ins[0]) {
+                        case "enqueue":
+                    obj.addLast(Integer.parseInt(ins[1]));
+                        break;
+                        case "push":
+                    obj.pushFront(Integer.parseInt(ins[1]));
+                    break;
+                        case "pop":
+                        if (obj.isEmpty()) {
+                System.out.println("Steque is empty.");
+                break;
+                        } else {
+                            obj.deleteLast();
+                            break;
+                        }
+                        default:
+                        break;
+                    }
+                } else {
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+    }
 }
