d13719b40698f3c6a14f21c26d5812a0217ed968
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-10-01 09:59:35 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ m7/7.1 Steque/Solution.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nooftestcases = Integer.parseInt(sc.nextLine());
+		int i = 0;
+		Steque obj = new Steque();
+		while (i < nooftestcases) {
+			while (sc.hasNext()) {
+				String[] ins = sc.nextLine().split(" ");
+				switch(ins[0]) {
+					case "enqueue":
+					obj.pushAtEnd(Integer.parseInt(ins[1]));
+					break;	
+					case "push":
+					obj.insertFront(Integer.parseInt(ins[1]));
+					break;
+					case "pop":
+					obj.deleteBack(Integer.parseInt(ins[1]));
+					break;
+					default:
+					break;
+				}
+			}
+			i++;
+		}
+	}
+}
---
+++ m7/7.1 Steque/Steque.java
@@ -0,0 +1,27 @@
+public class Steque<E>  {
+	E[] steque;
+	int head, tail, capacity;
+	public Steque() {
+		steque = (E[])new Object[100];
+		capacity = 2000;
+		head = 1000;
+		tail = 1000;
+	}
+	public void pushAtEnd(E item) {
+		if (tail < capacity) {
+			steque[tail++] = item;
+		}
+	}
+	public void insertFront(E item) {
+		if (head > 0) {
+			head--;
+			steque[head] = item;
+		}
+	}
+	public void deleteBack(E item) {
+		if (tail > head) {
+			tail--;
+			steque[tail] = null;
+		}
+	}
+}
