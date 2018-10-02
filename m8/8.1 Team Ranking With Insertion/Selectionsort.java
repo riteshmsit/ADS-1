6d5384ca9ab125974dae5f9cc9f5e83ff526151b
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-10-02 18:10:39 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ m8/8.1 Team Ranking With Insertion/Selectionsort.java
@@ -0,0 +1,45 @@
+class Selectionsort {
+    Selectionsort() {
+
+    }
+    public void sortByInsertion(Team[] a, int teamsize) {
+        int n = a.length;
+        int max = 0;
+        String s = "";
+        for (int i = 0; i < teamsize - 1; i++) { 
+            max = i; 
+            for (int j = i + 1; j < n; j++) {
+                if (a[j].getWins() > a[max].getWins()) {
+                    max = j;
+                } else if (a[j].getWins() == a[max].getWins()) {
+                    if (a[j].getLosses() < a[max].getLosses() && a[j].getLosses() != a[max].getLosses()) {
+                        max = j;
+                    } else if (a[j].getDraws() > a[max].getDraws()) {
+                        max = j;
+                    }
+                }
+            }
+            String s1 = a[max].getTeam();
+            String s2 = a[i].getTeam();
+            exchange(s1,s2);
+        }
+        displayToString(a);
+    }
+    public void display(int teamsize, Team[] a) {
+        System.out.println(a[teamsize].getTeam());
+    }
+    public void exchange(String s1, String s2) {
+        String temp = s1;
+        s1 = s2;
+        s2 = temp;
+    }
+    public void displayToString(Team[] a) {
+        String s = "";
+        int i;
+        for (i = 0; i < a.length - 1; i++) {
+            s += a[i].getTeam() + ",";
+        }
+            s += a[i].getTeam();
+        System.out.println(s);
+    }
+}
---
+++ m8/8.1 Team Ranking With Insertion/Team.java
@@ -0,0 +1,22 @@
+class Team {
+	String team;
+	int wins, losses, draws;
+	Team(String team, int wins, int losses, int draws) {
+		this.team = team;
+		this.wins = wins;
+		this.losses = losses;
+		this.draws = draws;
+	}
+	public String getTeam() {
+		return this.team;
+	}
+	public int getWins() {
+		return this.wins;
+	}
+	public int getLosses() {
+		return this.losses;
+	}
+	public int getDraws() {
+		return this.draws;
+	}
+}
