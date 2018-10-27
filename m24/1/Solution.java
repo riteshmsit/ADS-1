f639f8205bf3f5f642811b81fa3900d4026bfc53
riteshmsit <riteshveluvolurockz@msitprogram.net>
2018-10-27 12:49:05 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m24/1/Solution.java
+++ m24/1/Solution.java
@@ -4,21 +4,18 @@ class Solution {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
 		Studentdetails details = new Studentdetails();
-		//Binarysearchtree bst = new Binarysearchtree();
 		Hashtable hashing = new Hashtable();
+		// The time complexity is O(N)
 		for (int i = 0; i < n; i++) {
 			String[] st1 = sc.nextLine().split(",");
 			hashing.put(st1[0], new Studentdetails(st1[1], Double.parseDouble(st1[2])));
-			// details = new Studentdetails(Integer.parseInt(st1[1]), st1[2]);
-			// bst.put(details, Double.parseDouble(st1[2]));
 		}
-		int queries = Integer.parseInt(sc.nextLine());;
+		int queries = Integer.parseInt(sc.nextLine());
+		//The time complexity is O(N)
 		for (int i = 0; i < queries; i++) {
 			String[] st2 = sc.nextLine().split(" ");
 			switch(st2[0]) {
 				case "get":
-					// details = new Studentdetails(Integer.parseInt(st2[1]), Integer.parseInt(st2[2]));
-					// System.out.println(bst.get(details));
 					System.out.println(hashing.get(st2[1], Integer.parseInt(st2[2])));
 				break;
 				default:
